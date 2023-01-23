05173e951755926cdb58892ea7806219f7bf5ad8
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-01-23 11:51:12 +0100

    fix(ci): missing base

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import commonjs from "vite-plugin-commonjs";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [commonjs(), vue()],
-  // base: "/monaco-vue-demo/",
+  base: "/monaco-vue-demo/",
 });
